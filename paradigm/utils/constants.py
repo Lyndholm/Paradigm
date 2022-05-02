e1d45f4b9dc1e73b46db2549140c4e060ce0b088
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2022-05-02 14:12:48 +0300

    Items paths

---
+++ paradigm/utils/constants.py
@@ -0,0 +1,58 @@
+# Items definition
+OUTFITS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Characters/'
+
+BACKPACKS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Backpacks/'
+
+PICKAXES_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Pickaxes/'
+
+PICKAXES_ALT_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/PickAxes/'
+
+GLIDERS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Gliders/'
+
+SKYDIVING_TRAILS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Contrails/'
+
+EMOTES_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Dances/'
+
+ITEM_WRAPS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/ItemWraps/'
+
+PETS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/PetCarriers/'
+
+EMOJIS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Dances/Emoji/'
+
+SPRAYS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Sprays/'
+
+MUSIC_PACKS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/MusicPacks/'
+
+LAODING_SCREENS_PATH = 'FortniteGame/Content/Athena/Items/Cosmetics/Loadingscreens/'
+
+
+# Textures
+OUTFITS_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/T-AthenaSoldiers'
+
+BUNDLES_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Outfit/T-AthenaBundles'
+
+BACKPACKS_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Backpack/'
+
+PICKAXES_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Pickaxe/'
+
+PICKAXES_TEXTURES_ALT_PATH = 'FortniteGame/Content/UI/Foundation/Textures/Icons/Weapons/Items/'
+
+GLIDERS_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/BattleRoyale/FeaturedItems/Glider/'
+
+SKYDIVING_TRAILS_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/Icons/Skydiving/FX-Trails/'
+
+EMOTES_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/Icons/Emotes/'
+
+ITEM_WRAPS_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/Icons/Wraps/WeaponRenders/StaticRenders/'
+
+PETS_TEXTURES_PATH = 'FortniteGame/Content/UI/Foundation/Textures/Icons/Pets/'
+
+EMOJIS_TEXTURES_PATH = 'FortniteGame/Content/2dAssets/Emoji/Season20/'
+
+SPRAYS_TEXTURES_PATH = 'FortniteGame/Content/2dAssets/Sprays/Season20/PreviewImages/'
+
+MUSIC_PACKS_TEXTURES_PATH = 'FortniteGame/Content/2dAssets/Music/Season20/PreviewImages/'
+
+LAODING_SCREENS_TEXTURES_PATH = 'FortniteGame/Content/2dAssets/Loadingscreens/Season20/PreviewImages/'
+
+GAMEPLAY_FEATURES_ITEMS = 'FortniteGame/Plugins/GameFeatures/ResolveGameplay/Content/Icons/'
